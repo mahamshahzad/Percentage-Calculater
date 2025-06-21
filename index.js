e03e38c4adffed098167f9d8f483d3619d6e130c
Maham Shahzad <mahibutt0999@gmail.com>
2025-06-21 04:41:40 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+totalmarks = prompt ("Enter your total marks")
+obtainmarks = prompt ("Enter your obtain marks")
+
+percentage = (obtainmarks / totalmarks) * 
+
+alert("your percentage is" + percentage)
+document.write("your percentage is" + percentage)
+console.log("your percentage is" + percentage)
